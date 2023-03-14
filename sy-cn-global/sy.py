501b72857cfe49038b0ad193d686fee3d679fbdd
Yuhui Wei <1058369139@qq.com>
2023-03-14 13:17:39 +0800

    sync china and global

--- sy-cn-global/sy.py
+++ sy-cn-global/sy.py
@@ -6,32 +6,33 @@ import os
 # dataset_path = '/mnt/c/Users/ext_bc_rdsec_yuhuiw/Desktop/New folder/a-pipeline/yuhui-wei-rdsec/sy-cn-global/china.json'
 def china_json():
     china_path = os.path.dirname(__file__)
-
     with open(os.path.join(china_path, 'china.json')) as cn:
         data = json.load(cn)
-    china_mail = []
-    for mail in data["china"]:
-        china_mail.append(mail["mail"])
-    return china_mail
+    china_dl = []
+    for dl in data["china"]:
+        china_dl.append(dl["userPrincipalName"].split('@')[0])
+    return china_dl
 
 def global_json():
     global_path = os.path.dirname(__file__)
     with open(os.path.join(global_path, 'global.json')) as gl:
         data = json.load(gl)
-    global_mail = []
-    for mail in data["global"]:
-        global_mail.append(mail["Email"])
-    return global_mail
+    global_dl = []
+    for dl in data["global"]:
+        global_dl.append(dl["Member"].split('\\')[1])
+    return global_dl
 
 if __name__ == '__main__':
     cns= china_json()
+    # print(cns)
     gls = global_json()
+    # print(gls)
     
-    ret = []
-    for i in cns:
-        if i not in gls:
-            ret.append(i)
-    print(ret)
+    # ret = []
+    # for i in cns:
+    #     if i not in gls:
+    #         ret.append(i)
+    # print(ret)
     
     
     ret1 = []
