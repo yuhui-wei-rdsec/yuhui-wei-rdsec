8905390e80e7c25db1745e3a6a35b0f41ea8399b
Yuhui Wei <1058369139@qq.com>
2023-04-23 12:03:58 +0800

    wei

--- test-pipeline/t.py
+++ test-pipeline/t.py
@@ -9,5 +9,6 @@ data = {
 json_str = json.dumps(data)
 print(json_str)
 
+
 MailBody='Dear user, \n\nWelcome to ChinaSDZ, please refer to user guide to config your environment. \nUser guide link: https://wiki.jarvis.trendmicro.com/display/RDSEC/China+SDZ+User+Guideline \n\nFirst time, you must change your password.\nYour AAD Account credential is:\nUser name: {0}\nPassword: {1} \n\nBest Regards,\nCDC RDSec infra team'
 # print(MailBody)
